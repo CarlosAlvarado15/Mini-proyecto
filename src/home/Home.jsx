c244358d22afce8d456ed5d585815d2fbc9e6287
Carlos Alvarado <carlosjam10@gmail.com>
2023-06-19 11:10:24 -0500

    avance 50%

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,26 +4,26 @@ import "./home.css";
 export default function Home(props) {
   return (
     <div className="bloque1">
-    <div className="house">
-      <div className="imagen" key="i">
-        <img src={props.photo} alt="" />
-      </div>
-      <div className="detalles">
-        <div className="superHost" key="i">
-          {props.superHost} SUPER HOST
+      <div className="house">
+        <div className="imagen" key="i">
+          <img src={props.photo} alt="" />
         </div>
-        <div className="type" key="i">
-          {props.type}
+        <div className="detalles">
+          <div className="superHost" key="i">
+            {props.superHost} SUPER HOST
+          </div>
+          <div className="type" key="i">
+            {props.type}
+          </div>
+          <div className="rating" key="i">
+            <img src="src\img\star(1).svg" alt="star" height="20" width="19" />
+            {props.rating}
+          </div>
         </div>
-        <div className="rating" key="i">
-          <img src="src\img\star(1).svg" alt="star" height="20" width="19" />
-          {props.rating}
+        <div className="Title" key="i">
+          {props.title}
         </div>
       </div>
-      <div className="Title" key="i">
-        {props.title}
-      </div>
-    </div>
     </div>
   );
 }
--- src/nav/Nav.jsx
+++ src/nav/Nav.jsx
@@ -1,33 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Nav.css";
 function Nav() {
+  const [location, setLocation] = useState("");
+
+  const OPCIONES = [
+    { id: 1, city: "Helsinki, Finland" },
+    { id: 2, city: "Turku, Finland" },
+    { id: 3, city: "Oulu, Finland" },
+    { id: 4, city: "Vaasa, Finland" },
+  ];
+
+  function insertOptcion(parameter) {
+    setlocation(parameter);
+  }
   return (
-          <div id="App">
-        <div id="contenedor">
-          <div id="contenedor-centrado">
-            <nav>
-                <div>
-                  <p>
-                    <b>LOCATION</b>
-                  </p>
-                  <input type="text" defaultValue="Test" />
-                </div>
-                <div className="second-child">
-                  <p id="Location">
-                    <b>LOCATION</b>
-                  </p>
-                  <input type="text" defaultValue="Test" />
-                </div>
-                <div>
-                 <button> <img src="src\img\search_icon(4).svg" height={17.05} width={17.06} alt="lupa" />Search </button>
-                </div>
-              
-            </nav>
-            <div>{/* aqui colocar las opciones */}</div>
+    <div id="App">
+      <div id="contenedor">
+        <div id="contenedor-centrado">
+          <nav>
+            <div>
+              <p>
+                <b>LOCATION</b>
+              </p>
+              <input type="text" defaultValue={location} />
+            </div>
+            <div>
+              <input type="number" defaultValue="1" disabled />
+            </div>
+            <div>
+              <button>
+                <img
+                  src="src\img\search_icon(4).svg"
+                  height={17.05}
+                  width={17.06}
+                  alt="lupa"
+                />
+                Search
+              </button>
+            </div>
+          </nav>
+          <div id="div-toggles">
+            <div id="toggle1">
+              <ul>
+                {OPCIONES.map((op) => (
+                  <li key={op.id} onClick={() => insertOptcion(op.city)}>
+                    {op.city}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div id="toggle2"></div>
           </div>
         </div>
       </div>
-    
+    </div>
   );
 }
 
